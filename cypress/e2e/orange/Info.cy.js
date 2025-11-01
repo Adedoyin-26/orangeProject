2b18f7b9ee5346d1b9a931907f013e6a3b2f45b6
Adedoyin-26 <ibraheemadedoyin26@gmail.com>
2025-11-01 15:09:20 +0100

    forms interraction

--- cypress/e2e/orange/Info.cy.js
+++ cypress/e2e/orange/Info.cy.js
@@ -1,14 +1,12 @@
-/// <reference types="cypress" />   
+/// <reference types="cypress" />
 
 import Info from '../../pageObjects/Info';
 import Login from '../../pageObjects/Login';
 
-describe('Interact with Info page', () => {
+describe('Info Page - Complete Form Interactions', () => {
 
     beforeEach(() => {
-    
          cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
-
         cy.wait(5000);
 
         const lgn = new Login();
@@ -18,111 +16,435 @@ describe('Interact with Info page', () => {
             lgn.enterPassword(data.password);
             lgn.clickLogin();
             lgn.verifyLogin(data.expectedTitle);
-
             cy.wait(5000);
+        });
+    });
 
-});
-});
-    it('Interact with My Info', () => {
-
+    // ========== Personal Details Tests ==========
+    it('Should update Personal Details with all form fields', () => {
         const inf = new Info();
 
         cy.fixture('Orangehrm').then((data) => {
             inf.clickMyInfo();
-
             cy.wait(2000);
 
+            cy.scrollTo('top');
+            cy.wait(1000);
+
+            inf.enterFirstName(data.personalDetails.firstName);
+            inf.enterMiddleName(data.personalDetails.middleName);
+            inf.enterLastName(data.personalDetails.lastName);
+            inf.enterEmployeeId(data.personalDetails.employeeId);
+            inf.enterOtherId(data.personalDetails.otherId);
+            inf.enterDriverLicense(data.personalDetails.driverLicense);
+            inf.enterLicenseExpiryDate(data.personalDetails.licenseExpiryDate);
+
+            cy.scrollTo('center');
+            cy.wait(1000);
+
+            inf.selectNationality(data.personalDetails.nationality);
+            cy.wait(500);
+            inf.selectMaritalStatus(data.personalDetails.maritalStatus);
+            cy.wait(500);
+            inf.enterDateOfBirth(data.personalDetails.dateOfBirth);
+            inf.selectGender(data.personalDetails.gender);
+
+            cy.scrollTo('bottom');
+            cy.wait(1000);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    it('Should update only First Name and Last Name', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+
+            inf.enterFirstName(data.personalDetails.firstName);
+            inf.enterLastName(data.personalDetails.lastName);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+            inf.verifyFirstName(data.personalDetails.firstName);
+            inf.verifyLastName(data.personalDetails.lastName);
+        });
+    });
+
+    it('Should select Nationality and Marital Status', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+
+            cy.scrollTo('center');
+            cy.wait(1000);
+
+            inf.selectNationality(data.personalDetails.nationality);
+            cy.wait(500);
+            inf.selectMaritalStatus(data.personalDetails.maritalStatus);
+            cy.wait(500);
+
+            cy.scrollTo('bottom');
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    // ========== Contact Details Tests ==========
+    it('Should update Contact Details with all fields', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickContactInfo();
+            cy.wait(2000);
+
+            cy.scrollTo('top');
+            cy.wait(1000);
+
+            inf.enterStreet1(data.contactDetails.street1);
+            inf.enterStreet2(data.contactDetails.street2);
+            inf.enterCity(data.contactDetails.city);
+            inf.enterStateProvince(data.contactDetails.stateProvince);
+            inf.enterZipPostalCode(data.contactDetails.zipPostalCode);
+
+            cy.scrollTo('center');
+            cy.wait(1000);
+
+            inf.selectCountry(data.contactDetails.country);
+            cy.wait(500);
+
+            inf.enterHomeTelephone(data.contactDetails.homeTelephone);
+            inf.enterMobile(data.contactDetails.mobile);
+            inf.enterWorkTelephone(data.contactDetails.workTelephone);
+
+            cy.scrollTo('bottom');
+            cy.wait(1000);
+
+            inf.enterWorkEmail(data.contactDetails.workEmail);
+            inf.enterOtherEmail(data.contactDetails.otherEmail);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    it('Should update City and Mobile number', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickContactInfo();
+            cy.wait(2000);
+
+            inf.enterCity(data.contactDetails.city);
+            inf.enterMobile(data.contactDetails.mobile);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+            inf.verifyCity(data.contactDetails.city);
+            inf.verifyMobile(data.contactDetails.mobile);
+        });
+    });
+
+    // ========== Emergency Contacts Tests ==========
+    it('Should add Emergency Contact with all fields', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickEmergencyContacts();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.enterEmergencyName(data.emergencyContact.name);
+            inf.enterEmergencyRelationship(data.emergencyContact.relationship);
+            inf.enterEmergencyHomeTelephone(data.emergencyContact.homeTelephone);
+            inf.enterEmergencyMobile(data.emergencyContact.mobile);
+            inf.enterEmergencyWorkTelephone(data.emergencyContact.workTelephone);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+            inf.verifyEmergencyContactAdded(data.emergencyContact.name);
+        });
+    });
+
+    it('Should add Emergency Contact with required fields only', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickEmergencyContacts();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.enterEmergencyName(data.emergencyContact.name);
+            inf.enterEmergencyRelationship(data.emergencyContact.relationship);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    // ========== Dependents Tests ==========
+    it('Should add Dependent with all fields', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickDependents();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.enterDependentName(data.dependent.name);
+            inf.selectDependentRelationship(data.dependent.relationship);
+            cy.wait(500);
+            inf.enterDependentDateOfBirth(data.dependent.dateOfBirth);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+            inf.verifyDependentAdded(data.dependent.name);
+        });
+    });
+
+    it('Should add Dependent with Child relationship', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickDependents();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.enterDependentName('Sarah Johnson');
+            inf.selectDependentRelationship('Child');
+            cy.wait(500);
+            inf.enterDependentDateOfBirth('2015-15-03');
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    // ========== Immigration Tests ==========
+    it('Should add Immigration record with all fields', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickImmigration();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            cy.scrollTo('top');
+            cy.wait(1000);
+
+            inf.selectDocumentType(data.immigration.documentType);
+            cy.wait(500);
+            inf.enterImmigrationNumber(data.immigration.number);
+            inf.enterImmigrationIssuedDate(data.immigration.issuedDate);
+            inf.enterImmigrationExpiryDate(data.immigration.expiryDate);
+
+            cy.scrollTo('center');
+            cy.wait(1000);
+
+            inf.enterEligibleStatus(data.immigration.eligibleStatus);
+            inf.selectImmigrationCountry(data.immigration.country);
+            cy.wait(500);
+            inf.enterIssuedBy(data.immigration.issuedBy);
+            inf.enterReviewDate(data.immigration.reviewDate);
+
+            cy.scrollTo('bottom');
+            cy.wait(1000);
+
+            inf.enterImmigrationComments(data.immigration.comments);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+            inf.verifyImmigrationRecordAdded(data.immigration.number);
+        });
+    });
+
+    it('Should add Immigration record with Passport document type', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickImmigration();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.selectDocumentType('Passport');
+            cy.wait(500);
+            inf.enterImmigrationNumber('P987654321');
+            inf.enterImmigrationIssuedDate('2020-01-01');
+            inf.enterImmigrationExpiryDate('2030-31-12');
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    // ========== Membership Tests ==========
+    it('Should add Membership with all fields', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickMembership();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            cy.scrollTo('top');
+            cy.wait(1000);
+
+            inf.selectMembership(data.membership.membership);
+            cy.wait(500);
+            inf.selectSubscriptionPaidBy(data.membership.subscriptionPaidBy);
+            cy.wait(500);
+            inf.enterSubscriptionAmount(data.membership.subscriptionAmount);
+            inf.selectCurrency(data.membership.currency);
+            cy.wait(500);
+
+            cy.scrollTo('bottom');
+            cy.wait(1000);
+
+            inf.enterSubscriptionCommenceDate(data.membership.subscriptionCommenceDate);
+            inf.enterSubscriptionRenewalDate(data.membership.subscriptionRenewalDate);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+            inf.verifyMembershipAdded(data.membership.membership);
+        });
+    });
+
+    it('Should add Membership with Company paid subscription', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickMembership();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.selectMembership('British Computer Society (BCS)');
+            cy.wait(500);
+            inf.selectSubscriptionPaidBy('Company');
+            cy.wait(500);
+            inf.enterSubscriptionAmount('500');
+            inf.selectCurrency('United States Dollar');
+            cy.wait(500);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    it('Should add Membership with Individual paid subscription', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            inf.clickMyInfo();
+            cy.wait(2000);
+            inf.clickMembership();
+            cy.wait(2000);
+
+            inf.clickAddButton();
+            cy.wait(2000);
+
+            inf.selectMembership('Association for Computing Machinery (ACM)');
+            cy.wait(500);
+            inf.selectSubscriptionPaidBy('Individual');
+            cy.wait(500);
+            inf.enterSubscriptionAmount('250');
+            inf.selectCurrency('Euro');
+            cy.wait(500);
+
+            inf.clickSaveButton();
+            cy.wait(2000);
+            inf.verifySuccessMessage();
+        });
+    });
+
+    // ========== Navigation Tests ==========
+    it('Should verify all Info page sections are accessible', () => {
+        const inf = new Info();
+
+        cy.fixture('Orangehrm').then((data) => {
+            // Personal Details
+            inf.clickMyInfo();
+            cy.wait(2000);
             inf.verifyMyInfo(data.expectedHeader);
+
+            // Contact Details
+            inf.clickContactInfo();
+            cy.wait(2000);
+            inf.verifyMyContactDetails(data.expectedHeader1);
+
+            // Emergency Contacts
+            inf.clickEmergencyContacts();
+            cy.wait(2000);
+
+            // Dependents
+            inf.clickDependents();
+            cy.wait(2000);
+            inf.verifyDependents(data.expectedHeader3);
+
+            // Immigration
+            inf.clickImmigration();
+            cy.wait(2000);
+
+            // Membership
+            inf.clickMembership();
+            cy.wait(2000);
+            inf.verifyMembership(data.expectedHeader5);
+        });
     });
-});
-
-    it('Interact with Contact Details', () => {
-
-        const inf = new Info();
-
-        cy.fixture('Orangehrm').then((data) => {
-
-        inf.clickMyInfo();
-
-        cy.wait(2000);
-
-        inf.clickContactInfo();
-
-        cy.wait(2000);
-
-        inf.verifyMyContactDetails(data.expectedHeader1);
-    });
 
 });
 
-    it('Interact with Emergency Contacts', () => {
 
-        const inf = new Info();
-
-        cy.fixture('Orangehrm').then((data) => {
-
-        inf.clickMyInfo();
-
-        cy.wait(2000);
-
-        inf.clickEmergencyContacts();
-
-        cy.wait(2000);
-
-        inf.verifyEmergencyContacts(data.expectedHeader2);
-    });
-});
-
-    it('Interact with Dependents', () => {
-
-        const inf = new Info();
-
-        cy.fixture('Orangehrm').then((data) => {
-
-        inf.clickMyInfo();
-
-        cy.wait(2000);
-
-        inf.clickDependents();
-
-        cy.wait(2000);
-
-        inf.verifyDependents(data.expectedHeader3);
-    });
-});
-    it('Interact with Immigration', () => {
-
-        const inf = new Info();
-
-        cy.fixture('Orangehrm').then((data) => {
-
-        inf.clickMyInfo();
-
-        cy.wait(2000);
-
-        inf.clickImmigration();
-
-        cy.wait(2000);
-
-        inf.verifyImmigration(data.expectedHeader4);
-    });
-});
-
-    it('Interact with Membership', () => {
-
-        const inf = new Info();
-
-        cy.fixture('Orangehrm').then((data) => {
-
-        inf.clickMyInfo();
-
-        cy.wait(2000);
-
-        inf.clickMembership();
-
-        cy.wait(2000);
-
-        inf.verifyMembership(data.expectedHeader5);
-    });
-});
-});
--- cypress/pageObjects/Info.js
+++ cypress/pageObjects/Info.js
@@ -1,17 +1,86 @@
 class Info{
+// ========== Navigation Links ==========
 info = 'a[href="/web/index.php/pim/viewMyDetails"]';
 contact = 'a[href="/web/index.php/pim/contactDetails/empNumber/7"]';
 emergency = 'a[href="/web/index.php/pim/viewEmergencyContacts/empNumber/7"]';
+dependents = 'a[href="/web/index.php/pim/viewDependents/empNumber/7"]';
+immigration = 'a[href="/web/index.php/pim/viewImmigration/empNumber/7"]';
+membership = 'a[href="/web/index.php/pim/viewMemberships/empNumber/7"]';
+
+// ========== Headers ==========
 expectedHeader = '.oxd-text.oxd-text--h6.orangehrm-main-title';
 expectedHeader1 = '.oxd-text.oxd-text--h6.orangehrm-sub-title';
 expectedHeader2 = '.oxd-text.oxd-text--span';
-dependents = 'a[href="/web/index.php/pim/viewDependents/empNumber/7"]';
 expectedHeader3 = '.oxd-text.oxd-text--h6.orangehrm-main-title';
-immigration = 'a[href="/web/index.php/pim/viewImmigration/empNumber/7"]';
 expectedHeader4 = '.oxd-text.oxd-text--span';
-membership = 'a[href="/web/index.php/pim/viewMemberships/empNumber/7"]';
 expectedHeader5 = '.oxd-button.oxd-button--medium.oxd-button--text';
 
+// ========== Personal Details Form Fields ==========
+firstNameField = "input[name='firstName']";
+middleNameField = "input[name='middleName']";
+lastNameField = "input[name='lastName']";
+employeeIdField = '.oxd-input.oxd-input--active';
+otherIdField = ".oxd-input.oxd-input--active";
+driverLicenseField = "input[placeholder=\"Driver's License Number\"]";
+licenseExpiryDateField = "input[placeholder='yyyy-dd-mm']";
+nationalityDropdown = '.oxd-select-text-input';
+maritalStatusDropdown = '.oxd-select-text-input';
+dateOfBirthField = "input[placeholder='yyyy-dd-mm']";
+genderRadio = "input[type='radio']";
+
+// ========== Contact Details Form Fields ==========
+street1Field = "input[placeholder='Street 1']";
+street2Field = "input[placeholder='Street 2']";
+cityField = "input[placeholder='City']";
+stateProvinceField = "input[placeholder='State/Province']";
+zipPostalCodeField = "input[placeholder='Zip/Postal Code']";
+countryDropdown = '.oxd-select-text-input';
+homeTelephoneField = "input[placeholder='Home']";
+mobileField = "input[placeholder='Mobile']";
+workTelephoneField = "input[placeholder='Work']";
+workEmailField = "input[placeholder='Work Email']";
+otherEmailField = "input[placeholder='Other Email']";
+
+// ========== Emergency Contacts Form Fields ==========
+emergencyNameField = "input[placeholder='Name']";
+emergencyRelationshipField = "input[placeholder='Relationship']";
+emergencyHomeTelephoneField = "input[placeholder='Home Telephone']";
+emergencyMobileField = "input[placeholder='Mobile']";
+emergencyWorkTelephoneField = "input[placeholder='Work Telephone']";
+
+// ========== Dependents Form Fields ==========
+dependentNameField = "input[placeholder='Name']";
+dependentRelationshipDropdown = '.oxd-select-text-input';
+dependentDateOfBirthField = "input[placeholder='yyyy-dd-mm']";
+
+// ========== Immigration Form Fields ==========
+immigrationNumberField = "input[placeholder='Number']";
+immigrationIssuedDateField = "input[placeholder='yyyy-dd-mm']";
+immigrationExpiryDateField = "input[placeholder='yyyy-dd-mm']";
+immigrationEligibleStatusField = "input[placeholder='Eligible Status']";
+immigrationIssuedByField = "input[placeholder='Issued By']";
+immigrationReviewDateField = "input[placeholder='yyyy-dd-mm']";
+immigrationCommentsField = "textarea[placeholder='Comments']";
+documentTypeDropdown = '.oxd-select-text-input';
+countryDropdownImmigration = '.oxd-select-text-input';
+
+// ========== Membership Form Fields ==========
+membershipDropdown = '.oxd-select-text-input';
+subscriptionPaidByDropdown = '.oxd-select-text-input';
+subscriptionAmountField = "input[placeholder='Subscription Amount']";
+currencyDropdown = '.oxd-select-text-input';
+subscriptionCommenceDateField = "input[placeholder='yyyy-dd-mm']";
+subscriptionRenewalDateField = "input[placeholder='yyyy-dd-mm']";
+
+// ========== Common Elements ==========
+saveButton = "button[type='submit']";
+addButton = '.oxd-button.oxd-button--medium.oxd-button--secondary';
+cancelButton = "button[type='button']";
+deleteButton = '.oxd-icon.bi-trash';
+editButton = '.oxd-icon.bi-pencil-fill';
+successMessage = '.oxd-toast.oxd-toast--success';
+
+// ========== Navigation Methods ==========
 clickMyInfo(){
     cy.get(this.info).click();
 }
@@ -33,15 +102,15 @@ clickEmergencyContacts(){
 }
 
 verifyEmergencyContacts(expectedName2){
-    cy.get(this.expectedHeader2).should('exist', expectedName2); 
-}  
+    cy.get(this.expectedHeader2).should('exist', expectedName2);
+}
 
 clickDependents(){
     cy.get(this.dependents).click();
 }
 
 verifyDependents(expectedName3){
-    cy.get(this.expectedHeader3).should('contain.text', expectedName3); 
+    cy.get(this.expectedHeader3).should('contain.text', expectedName3);
 }
 
 clickImmigration(){
@@ -58,6 +127,249 @@ clickMembership(){
 
 verifyMembership(expectedName5){
     cy.get(this.expectedHeader5).should('contain.text', expectedName5);
+}
+
+// ========== Personal Details Form Methods ==========
+enterFirstName(firstName){
+    cy.get(this.firstNameField).clear().type(firstName);
+}
+
+enterMiddleName(middleName){
+    cy.get(this.middleNameField).clear().type(middleName);
+}
+
+enterLastName(lastName){
+    cy.get(this.lastNameField).clear().type(lastName);
+}
+
+enterEmployeeId(employeeId){
+    cy.get(this.employeeIdField).eq(1).clear().type(employeeId);
+}
+
+enterOtherId(otherId){
+    cy.get(this.otherIdField).clear().type(otherId);
+}
+
+enterDriverLicense(licenseNumber){
+    cy.get(this.driverLicenseField).clear().type(licenseNumber);
+}
+
+enterLicenseExpiryDate(expiryDate){
+    cy.get(this.licenseExpiryDateField).eq(0).clear().type(expiryDate);
+}
+
+selectNationality(nationality){
+    cy.get(this.nationalityDropdown).eq(0).click();
+    cy.contains('.oxd-select-option', nationality).click();
+}
+
+selectMaritalStatus(status){
+    cy.get(this.maritalStatusDropdown).eq(1).click();
+    cy.contains('.oxd-select-option', status).click();
+}
+
+enterDateOfBirth(dob){
+    cy.get(this.dateOfBirthField).eq(1).clear().type(dob);
+}
+
+selectGender(gender){
+    if(gender === 'Male'){
+        cy.get(this.genderRadio).eq(0).check({force: true});
+    } else if(gender === 'Female'){
+        cy.get(this.genderRadio).eq(1).check({force: true});
+    }
+}
+
+clickSaveButton(){
+    cy.get(this.saveButton).eq(0).click();
+}
+
+verifySuccessMessage(){
+    cy.get(this.successMessage).should('be.visible');
+}
+
+verifyFirstName(expectedFirstName){
+    cy.get(this.firstNameField).should('have.value', expectedFirstName);
+}
+
+verifyLastName(expectedLastName){
+    cy.get(this.lastNameField).should('have.value', expectedLastName);
+}
+
+// ========== Contact Details Form Methods ==========
+enterStreet1(street1){
+    cy.get(this.street1Field).clear().type(street1);
+}
+
+enterStreet2(street2){
+    cy.get(this.street2Field).clear().type(street2);
+}
+
+enterCity(city){
+    cy.get(this.cityField).clear().type(city);
+}
+
+enterStateProvince(state){
+    cy.get(this.stateProvinceField).clear().type(state);
+}
+
+enterZipPostalCode(zip){
+    cy.get(this.zipPostalCodeField).clear().type(zip);
+}
+
+selectCountry(country){
+    cy.get(this.countryDropdown).eq(0).click();
+    cy.contains('.oxd-select-option', country).click();
+}
+
+enterHomeTelephone(phone){
+    cy.get(this.homeTelephoneField).clear().type(phone);
+}
+
+enterMobile(mobile){
+    cy.get(this.mobileField).clear().type(mobile);
+}
+
+enterWorkTelephone(workPhone){
+    cy.get(this.workTelephoneField).clear().type(workPhone);
+}
+
+enterWorkEmail(email){
+    cy.get(this.workEmailField).clear().type(email);
+}
+
+enterOtherEmail(email){
+    cy.get(this.otherEmailField).clear().type(email);
+}
+
+verifyCity(expectedCity){
+    cy.get(this.cityField).should('have.value', expectedCity);
+}
+
+verifyMobile(expectedMobile){
+    cy.get(this.mobileField).should('have.value', expectedMobile);
+}
+
+// ========== Emergency Contacts Form Methods ==========
+clickAddButton(){
+    cy.get(this.addButton).click();
+}
+
+enterEmergencyName(name){
+    cy.get(this.emergencyNameField).type(name);
+}
+
+enterEmergencyRelationship(relationship){
+    cy.get(this.emergencyRelationshipField).type(relationship);
+}
+
+enterEmergencyHomeTelephone(phone){
+    cy.get(this.emergencyHomeTelephoneField).type(phone);
+}
+
+enterEmergencyMobile(mobile){
+    cy.get(this.emergencyMobileField).type(mobile);
+}
+
+enterEmergencyWorkTelephone(workPhone){
+    cy.get(this.emergencyWorkTelephoneField).type(workPhone);
+}
+
+verifyEmergencyContactAdded(name){
+    cy.contains('.oxd-table-cell', name).should('be.visible');
+}
+
+// ========== Dependents Form Methods ==========
+enterDependentName(name){
+    cy.get(this.dependentNameField).type(name);
+}
+
+selectDependentRelationship(relationship){
+    cy.get(this.dependentRelationshipDropdown).eq(0).click();
+    cy.contains('.oxd-select-option', relationship).click();
+}
+
+enterDependentDateOfBirth(dob){
+    cy.get(this.dependentDateOfBirthField).type(dob);
+}
+
+verifyDependentAdded(name){
+    cy.contains('.oxd-table-cell', name).should('be.visible');
+}
+
+// ========== Immigration Form Methods ==========
+selectDocumentType(docType){
+    cy.get(this.documentTypeDropdown).eq(0).click();
+    cy.contains('.oxd-select-option', docType).click();
+}
+
+enterImmigrationNumber(number){
+    cy.get(this.immigrationNumberField).type(number);
+}
+
+enterImmigrationIssuedDate(date){
+    cy.get(this.immigrationIssuedDateField).eq(0).type(date);
+}
+
+enterImmigrationExpiryDate(date){
+    cy.get(this.immigrationExpiryDateField).eq(1).type(date);
+}
+
+enterEligibleStatus(status){
+    cy.get(this.immigrationEligibleStatusField).type(status);
+}
+
+selectImmigrationCountry(country){
+    cy.get(this.countryDropdownImmigration).eq(1).click();
+    cy.contains('.oxd-select-option', country).click();
+}
+
+enterIssuedBy(issuedBy){
+    cy.get(this.immigrationIssuedByField).type(issuedBy);
+}
+
+enterReviewDate(date){
+    cy.get(this.immigrationReviewDateField).eq(2).type(date);
+}
+
+enterImmigrationComments(comments){
+    cy.get(this.immigrationCommentsField).type(comments);
+}
+
+verifyImmigrationRecordAdded(number){
+    cy.contains('.oxd-table-cell', number).should('be.visible');
+}
+
+// ========== Membership Form Methods ==========
+selectMembership(membership){
+    cy.get(this.membershipDropdown).eq(0).click();
+    cy.contains('.oxd-select-option', membership).click();
+}
+
+selectSubscriptionPaidBy(paidBy){
+    cy.get(this.subscriptionPaidByDropdown).eq(1).click();
+    cy.contains('.oxd-select-option', paidBy).click();
+}
+
+enterSubscriptionAmount(amount){
+    cy.get(this.subscriptionAmountField).type(amount);
+}
+
+selectCurrency(currency){
+    cy.get(this.currencyDropdown).eq(2).click();
+    cy.contains('.oxd-select-option', currency).click();
+}
+
+enterSubscriptionCommenceDate(date){
+    cy.get(this.subscriptionCommenceDateField).eq(0).type(date);
+}
+
+enterSubscriptionRenewalDate(date){
+    cy.get(this.subscriptionRenewalDateField).eq(1).type(date);
+}
+
+verifyMembershipAdded(membership){
+    cy.contains('.oxd-table-cell', membership).should('be.visible');
 }
 
 }
